6d21ea426c167e1316ee668a38069dacc5b8e288
Tairan Wang <tairan.wang@gmail.com>
2018-09-02 00:53:33 +0800

    set default host uri

--- src/Amap.WebApi/Config.cs
+++ src/Amap.WebApi/Config.cs
@@ -4,6 +4,12 @@ namespace Amap.WebApi
 {
     public class Config
     {
+        public Config(string key)
+            : this("https://restapi.amap.com/", key)
+        {
+
+        }
+
         public Config(string host, string key)
         {
             Host = host;
